691f58af93274f384d0f8d164922691fd1b0f87c
PhaneendhraCH <phaneendhra.chinta@thundersoft.com>
2021-12-31 19:24:00 +0530

    deleted file (Integrated)

---
+++ add_module/delete.c
@@ -0,0 +1,9 @@
+#include "header.h"
+
+void delete(){
+
+	printf("Performing Delete Operation\n");
+	printf("Quitting delete \n");
+	Start_Menu();
+	//Printf("Employee Details Successfully deleted into Excel File\n");
+}
