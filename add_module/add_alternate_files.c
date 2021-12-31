6d4a73084a5fc3b0c1d7c65d7bac4ee1685ebac7
PhaneendhraCH <phaneendhra.chinta@thundersoft.com>
2021-12-31 19:23:39 +0530

    add file (Integrated)

---
+++ add_module/add_alternate_files.c
@@ -0,0 +1,140 @@
+/*	
+	Add Employee Details to Excel Sheet
+	
+	Execute :
+		make 
+*/
+
+#include "header.h"
+const char *timestamp(){
+			
+	char *buffer = malloc(max_size);
+	time_t t = time(NULL);
+	struct tm  *tm;
+	tm= localtime(&t); 
+			  
+	if ((tm->tm_hour) <12){
+			  	
+		sprintf(buffer,"%02d/%02d/%d %02d:%02d am", tm->tm_mday , tm->tm_mon + 1, tm->tm_year+ 1900, tm->tm_hour, tm->tm_min);
+		//printf("%02d/%02d/%d %02d:%02d am", tm->tm_mday , tm->tm_mon + 1, tm->tm_year+ 1900, tm->tm_hour, tm->tm_min);
+	}
+	else if ((tm->tm_hour)  == 12){
+		sprintf(buffer,"%02d/%02d/%d %02d:%02d pm", tm->tm_mday , tm->tm_mon + 1, tm->tm_year+ 1900, tm->tm_hour, tm->tm_min);
+		//printf("%02d/%02d/%d %02d:%02d pm", tm->tm_mday , tm->tm_mon + 1, tm->tm_year+ 1900, tm->tm_hour, tm->tm_min);
+	}
+	else{
+		sprintf(buffer,"%02d/%02d/%d %02d:%02d pm", tm->tm_mday , tm->tm_mon + 1, tm->tm_year+ 1900, (tm->tm_hour - 12), tm->tm_min);
+		//printf("%02d/%02d/%d %02d:%02d pm", tm->tm_mday , tm->tm_mon + 1, tm->tm_year+ 1900, (tm->tm_hour - 12), tm->tm_min);
+	}
+	
+	return buffer;
+}
+
+
+void add()
+{
+	printf("Performing Add Operation\n");
+	struct employee *s1;
+	s1 = malloc(sizeof(struct employee));
+	
+	FILE *fp1;
+	
+	printf("\nEnter Employee ID : ");
+	scanf("%s",s1->id);
+	
+	printf("\nEnter Name of the Employee : ");
+	getchar();
+	scanf("%[^\n]s",s1->name);
+	
+	printf("\nEnter Employee Email ID : ");
+	scanf("%s",s1->email);
+	
+	printf("\nEnter Band : ");
+	scanf("%s",s1->band);
+	
+	printf("\nEnter Date of Joining : ");
+	scanf("%s",s1->date);
+	
+	printf("\nEmployee Mobile Number : ");
+	scanf("%s",s1->mobile);
+	
+	printf("\nEnter Detials of reporting Manager : ");
+	getchar();
+	scanf("%[^\n]s",s1->manager);
+	
+	char repo;
+	char totalreportees[max_size]="";
+	printf("\nAny Reportees working under you : Yes(Y)/No(N) : ");
+	scanf(" %c",&repo);
+	
+	if (repo == 'Y'){
+		
+		
+		printf("\nEnter total no. of reporteees : ");
+		int total_reportee;
+		scanf("%d",&total_reportee);
+		int i;
+		for (i=0; i<total_reportee; i++){
+			char new_entry_reportee[max_size];
+			getchar();
+			printf("\nEnter Reportee Name and ID : ");
+			scanf("%[^\n]s",new_entry_reportee);
+			strcat(new_entry_reportee,"/");
+			strcat(totalreportees,new_entry_reportee);
+		}
+		totalreportees[strlen(totalreportees)-1] = '\0';
+	}
+	
+	else{
+		strcpy(totalreportees,"NA");
+	}
+		
+	printf("\nEnter Tech Area of Employee : ");
+	scanf("%s",s1->techarea);
+	
+	printf("\nProject Information : ");
+	getchar();
+	scanf("%[^\n]s",s1->project);
+	
+	
+	strcpy(s1->status,"Active");			//implicit initialisation
+	strcpy(s1->employee_relieving_date,"");	//implicit initialisation
+	
+	fp1 = fopen("sample_data.xlsx","a");
+	
+	strcpy(s1->date_employee_created,timestamp());
+	
+	fprintf(
+		fp1,
+		"%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+		s1->id,
+		s1->name,
+		s1->email,
+		s1->band,
+		s1->date,
+		s1->date_employee_created,
+		s1->mobile,
+		s1->manager,
+		s1->techarea,
+		s1->project,
+		s1->status,
+		totalreportees,
+		s1->employee_relieving_date
+	);
+	
+	fclose(fp1);
+	free(s1);
+	
+	printf("Employee Details Successfully added into Excel File\n");
+	Start_Menu();
+}
+
+/*
+int main(){
+
+	printf("Insert Thudersoft Employee Details\n");
+	add();
+	return 0;
+}*/
+
+
